9c1e397309256c87619305f1be1cb02cd7297e22
Simon Kaufmann <simon.k1996@gmail.com>
2019-08-09 09:24:12 +0100

    Queen can be moved to mouse click, but a bit off

---
+++ Assets/Scripts/Pieces.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pieces : MonoBehaviour
+{
+
+    public GameObject wQueen;
+
+    string tagChessBoard = "Table";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rend = wQueen.GetComponent<Renderer>();
+        rend.material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mouse down");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+            }
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name.Equals(tagChessBoard))
+                {
+                    Debug.Log("Table");
+                    wQueen.transform.position = hit.point;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## Useful links
 
-https://www.turbosquid.com/FullPreview/Index.cfm/ID/1408325
+https://www.turbosquid.com/FullPreview/Index.cfm/ID/1408325  
+https://answers.unity.com/questions/247810/how-to-get-gameobject-that-is-clicked-by-a-mouse.html  
+https://docs.unity3d.com/ScriptReference/Input.GetMouseButtonDown.html  
+https://answers.unity.com/questions/1065971/how-do-you-detect-a-mouse-button-click-on-a-game-o.html  
